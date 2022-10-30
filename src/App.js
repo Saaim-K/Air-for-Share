cb3ca293134137f117a915bdfd601be674eb4222
Saaim-K <saaimkhan221@gmail.com>
2022-10-30 23:49:00 +0500

    CRUD

--- src/App.js
+++ src/App.js
@@ -37,9 +37,8 @@ function App() {
 
     try {
       const docRef = await addDoc(collection(db, "users"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
+        text: postText,
+        createdOn: new Date().getTime(),
       });
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
