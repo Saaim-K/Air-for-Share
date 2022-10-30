6fef3ae513a17b58ad5bb1d8d21a83792b010284
Saaim-K <saaimkhan221@gmail.com>
2022-10-30 07:11:02 +0500

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [post, setPost] = useState('')
+
+  useEffect(() => {
+
+  }, [])
+
+  const share = (e) => {
+    e.preventDefault();
+    console.log('post');
+  }
+
   return (
-    <div>
-     
-    </div>
+    <>
+      <form onSubmit={share}>
+        <input type="text" placeholder='enter any text' onChange={(e) => {
+          setPost(e.target.value)
+        }} />
+
+      </form>
+    </>
   );
 }
 
