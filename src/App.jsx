47006c7994c1d0885b345b58011a478eb8259fac
Saaim-K <saaimkhan221@gmail.com>
2022-11-09 23:49:28 +0500

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Gallery from './Components/Gallery/Gallery';
 
 function App() {
   return (
     <>
-
+      <Home />
+      <About />
+      <Gallery />
     </>
   );
 }
