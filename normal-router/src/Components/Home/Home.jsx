13a875e12dd171d60e370f5e46224c1796c0d67e
Saaim-K <saaimkhan221@gmail.com>
2022-11-11 20:59:13 +0500

    Router

--- normal-router/src/Components/Home/Home.jsx
+++ normal-router/src/Components/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate,Link } from 'react-router-dom'
 
 function Home() {
-  let aboutNavigate = useNavigate();
+  // let aboutNavigate = useNavigate();
   let galleryNavigate = useNavigate();
   return (
     <>
       <h1>This is Home Page</h1>
-      <button onClick={() => { aboutNavigate('/about/:username') }}>Redirect to About page</button>
-      <button onClick={() => { galleryNavigate('/gallery') }}>Redirect to Gallery page</button>
+      <Link to='/about/:username'><button>Redirect to About page</button></Link>{/* Declarative Programmin */}
+      <button onClick={() => { galleryNavigate('/gallery') }}>Redirect to Gallery page</button>{/* Imperative Programmin */}
     </>
   )
 }
---
+++ normal-router/src/Components/Login/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <>
+      <h1>This is SignUp Page</h1>
+    </>
+  )
+}
+
+export default Signup
